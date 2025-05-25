2b05738b24b49fbdc8e98a44587b77d77a1b3602
Ignas Naulis <ignas.naulis@gmail.com>
2025-05-25 15:10:03 +0300

    new

--- src/components/Careers/Welcome.jsx
+++ src/components/Careers/Welcome.jsx
@@ -1,17 +1,27 @@
-import image from '../../assets/careers/image.png';
-import image2 from '../../assets/hero/Abstract Design (3).svg';
+import image from "../../assets/careers/image.png";
+import image2 from "../../assets/hero/Abstract Design (3).svg";
 
 export default function Welcome() {
-    return (
-        <div className="welcome-container">
-            <img src={image2} className='image2' alt="" />
-            <div className="welcome-text">
-                <h2>Welcome to <span className="green">YourBank</span> Careers!</h2>
-                <p>Join our team and embark on a rewarding journey in the banking industry. At YourBank, we are committed to fostering a culture of excellence and providing opportunities for professional growth. With a focus on innovation, customer service, and integrity, we strive to make a positive impact in the lives of our customers and communities. Join us today and be a part of our mission to shape the future of banking.</p>
-            </div>
-            <div className='welcome-image'>
-                <img src={image} alt="image" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="welcome-container">
+      <img src={image2} className="image2" alt="" />
+      <div className="welcome-text">
+        <h2>
+          Welcome to <span className="green">YourBank</span> Careers!
+        </h2>
+        <p>
+          Join our team and embark on a rewarding journey in the banking
+          industry. At YourBank, we are committed to fostering a culture of
+          excellence and providing opportunities for professional growth. With a
+          focus on innovation, customer service, and integrity, we strive to
+          make a positive impact in the lives of our customers and communities.
+          Join us today and be a part of our mission to shape the future of
+          banking.
+        </p>
+      </div>
+      <div className="welcome-image">
+        <img src={image} alt="image" />
+      </div>
+    </div>
+  );
 }
--- src/components/Home/testimonial/testimonial.jsx
+++ src/components/Home/testimonial/testimonial.jsx
@@ -1,53 +1,69 @@
-import { useState } from 'react';
-import arl from '../../../assets/home/Button.svg';
-import arr from '../../../assets/home/Button (1).svg';
-import TestimonialCard from './testimonialCard';
-import { testCardBuisness, testCardIndividuals } from '../../../api/data';
-import Header from '../../Header';
+import { useEffect, useState } from "react";
+import arl from "../../../assets/home/Button.svg";
+import arr from "../../../assets/home/Button (1).svg";
+import TestimonialCard from "./testimonialCard";
+import { testCardBuisness, testCardIndividuals } from "../../../api/data";
+import Header from "../../Header";
 
 export default function Testimonial() {
+  const [showIndividuals, setShowIndividuals] = useState(true);
+  const showCard = showIndividuals ? testCardIndividuals : testCardBuisness;
+  const [mobiel, setMobile] = useState(false);
 
-    const [showIndividuals, setShowIndividuals] = useState(true);
-    const showCard = showIndividuals ? testCardIndividuals : testCardBuisness;
+  useEffect(() => {
+    const handlewidth = () => {
+      setMobile(window.innerWidth <= 850);
+    };
 
-    const shownCardsNum = 3;
-    const [currentIndex, setCurrentIndex] = useState(0);
+    window.addEventListener("resize", handlewidth);
 
-    const next = () => {
-        if (currentIndex + shownCardsNum < showCard.length) {
-            setCurrentIndex(p => p + 1)
-        } else {
-            setCurrentIndex(0)
-        }
+    return () => window.removeEventListener("resize", handlewidth);
+  }, []);
+  const shownCardsNum = mobiel ? 1 : 3;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const next = () => {
+    if (currentIndex + shownCardsNum < showCard.length) {
+      setCurrentIndex((p) => p + 1);
+    } else {
+      setCurrentIndex(0);
     }
+  };
 
-    const prev = () => {
-        if (currentIndex > 0) {
-            setCurrentIndex(p => p - 1)
-        } else {
-            setCurrentIndex(showCard.length - shownCardsNum)
-        }
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((p) => p - 1);
+    } else {
+      setCurrentIndex(showCard.length - shownCardsNum);
     }
+  };
 
-
-    return (
-        <div className="test-wrap">
-            <Header
-                showIndividuals={showIndividuals}
-                setShowIndividuals={setShowIndividuals}
-                name={'Our'} nameGreen2={'Testimonials'}
-                para={'Discover how YourBank has transformed lives with innovative digital solutions and personalized customer service. See why our clients trust us for a secure and prosperous financial journey'}
-                btn={true}
-            />
-            <div className="test-slider">
-                <img src={arl} alt="arrow left" onClick={prev} />
-                {
-                    showCard.slice(currentIndex, currentIndex + shownCardsNum).map((i, k) => (
-                        <TestimonialCard t={i.t} name={i.name} key={k} />
-                    ))
-                }
-                <img src={arr} alt="arrow right" onClick={next} />
-            </div>
+  return (
+    <div className="test-wrap">
+      <Header
+        showIndividuals={showIndividuals}
+        setShowIndividuals={setShowIndividuals}
+        name={"Our"}
+        nameGreen2={"Testimonials"}
+        para={
+          "Discover how YourBank has transformed lives with innovative digital solutions and personalized customer service. See why our clients trust us for a secure and prosperous financial journey"
+        }
+        btn={true}
+      />
+      <div className="test-slider">
+        <div className="sliders">
+          {showCard
+            .slice(currentIndex, currentIndex + shownCardsNum)
+            .map((i, k) => (
+              <TestimonialCard t={i.t} name={i.name} key={k} />
+            ))}
         </div>
-    );
+
+        <div className="arrows">
+          <img src={arl} alt="arrow left" onClick={prev} />
+          <img src={arr} alt="arrow right" onClick={next} />
+        </div>
+      </div>
+    </div>
+  );
 }
